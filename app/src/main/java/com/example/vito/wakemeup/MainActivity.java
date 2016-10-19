2852ec98ccc5b278f9af9f971e72215aa98c474b
jeanTindel <jean.tindel@gmail.com>
2016-10-19 14:03:21 +0200

    Merge branch 'master' of https://github.com/strabaud/WakeMeUp
    # Conflicts:
    #	.gradle/2.14.1/taskArtifacts/cache.properties.lock
    #	.gradle/2.14.1/taskArtifacts/fileHashes.bin
    #	.gradle/2.14.1/taskArtifacts/fileSnapshots.bin
    #	.gradle/2.14.1/taskArtifacts/fileSnapshotsToTreeSnapshotsIndex.bin
    #	.gradle/2.14.1/taskArtifacts/taskArtifacts.bin
    #	.gradle/2.14.1/tasks/_app_compileDebugJavaWithJavac/localClassSetAnalysis/localClassSetAnalysis.bin
    #	.gradle/2.14.1/tasks/_app_compileDebugJavaWithJavac/localClassSetAnalysis/localClassSetAnalysis.lock

--- app/src/main/java/com/example/vito/wakemeup/MainActivity.java
+++ app/src/main/java/com/example/vito/wakemeup/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.vito.wakemeup;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.view.View;
 import android.content.Intent;
 import android.widget.ImageButton;
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     // create a editText to get the click option
-    EditText WeekDaysEdit;
+    TextView WeekDaysEdit;
     ImageButton SettingsEdit;
     ArrayList<Time> alarms;
 
@@ -26,11 +26,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         setContentView(R.layout.activity_main);
 
-        WeekDaysEdit = (EditText)findViewById(R.id.WeekDays);
+        WeekDaysEdit = (TextView)findViewById(R.id.WeekDays);
         WeekDaysEdit.setOnClickListener(this);
 
         SettingsEdit = (ImageButton)findViewById(R.id.imageButton1);
         SettingsEdit.setOnClickListener(this);
+
+        DisplayAlarms();
 
     }
 
@@ -43,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent SetTimerActivity = new Intent(this,TimerActivity.class);
             //on passe l'intention au système
             startActivity(SetTimerActivity);
+            return;
 
         }
         else if(R.id.imageButton1 == view.getId()){
@@ -56,18 +59,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Log.e("Bouton","clic pas implémenté !");
         }
     }
-    /*
+
     @Override
     public void onResume()
     {
         super.onResume();
+        DisplayAlarms();
+    }
+
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        DisplayAlarms();
+    }
+
+    public void DisplayAlarms(){
         alarms = Alarms.getInstance().getAlarms();
         Time t1 = new Time(0,0);
-        t1 = alarms.get(0);
-        String hour  =  Integer.toString(t1.hour);
-        String min = Integer.toString(t1.minutes);
 
-        WeekDaysEdit.setText(hour + " : " + min);
+        if(alarms!=null)
+        {
+            t1 = alarms.get(0);
+            String hour  =  Integer.toString(t1.hour);
+            String min = Integer.toString(t1.minutes);
+            WeekDaysEdit.setText(hour + " : " + min);
+        }
     }
-*/
+
 }
