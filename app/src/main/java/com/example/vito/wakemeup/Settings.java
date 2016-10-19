bf963d6e64b7c0ea8e43633c238af9d17050e04c
strabaud <syl_ouf@msn.com>
2016-10-19 15:36:44 +0200

    settings bt_OK ok

--- app/src/main/java/com/example/vito/wakemeup/Settings.java
+++ app/src/main/java/com/example/vito/wakemeup/Settings.java
@@ -2,12 +2,33 @@ package com.example.vito.wakemeup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class Settings extends AppCompatActivity {
+public class Settings extends AppCompatActivity implements View.OnClickListener{
+
+    private Button OkButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        OkButton = (Button)findViewById(R.id.bt_Ok);
+        OkButton.setOnClickListener(this);
+    }
+
+    public void onClick(View view)
+    {
+
+        if(R.id.bt_Ok == view.getId())
+        {
+            this.finish();
+        }
+        else
+        {
+            Log.e("Bouton","clic pas implémenté !");
+        }
     }
 }
