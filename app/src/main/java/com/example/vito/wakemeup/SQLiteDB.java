1cd207384912ef48b3f9f936e81d30585d0472ae
strabaud <syl_ouf@msn.com>
2016-10-26 14:06:39 +0200

    GESTION DES TEMPS DODO EN DB SQL

--- app/src/main/java/com/example/vito/wakemeup/SQLiteDB.java
+++ app/src/main/java/com/example/vito/wakemeup/SQLiteDB.java
@@ -23,12 +23,23 @@ public class SQLiteDB extends SQLiteOpenHelper {
     private static final String COL_ACTIVITY3 = "ACTIVITY3";
 
     // VARIABLES POUR TABLE HEURES DE SOMMEIL
-
+    private static final String TABLE_SLEEP = "table_sleep";
+    private static final String SLEEP_ID = "ID";
+    //Variable numéro de semaine
+    private static final String SLEEP_WEEK = "WEEK";
+    //variables heure couché/heure réveil
+    private static final String SLEEP_TIME1 = "TIME1";
+    private static final String SLEEP_TIME2 = "TIME2";
 
     //STRING REQUETE CREATION HOBBIES
     private static final String CREATE_DB_HOBBIES = "CREATE TABLE " + TABLE_ACTIVITIES + " ("
             + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_ACTIVITY1 + " TEXT NOT NULL, "
             + COL_ACTIVITY2 + " TEXT NOT NULL, "+ COL_ACTIVITY3 + " TEXT NOT NULL );";
+
+    //STRING REQUETE CREATION HEURES DORMIES
+    private static final String CREATE_DB_SLEEP = "CREATE TABLE " + TABLE_SLEEP + " ("
+            + SLEEP_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + SLEEP_WEEK + " INTEGER NOT NULL, "
+            + SLEEP_TIME1 + " TEXT NOT NULL, "+ SLEEP_TIME2 + " TEXT NOT NULL );";
 
     public SQLiteDB(Context context, String name, CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -38,6 +49,7 @@ public class SQLiteDB extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         //Création table à partir de CREATE_DB_HOBBIES
         db.execSQL(CREATE_DB_HOBBIES);
+        db.execSQL(CREATE_DB_SLEEP);
     }
 
     @Override
